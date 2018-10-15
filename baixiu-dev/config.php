b6c0b2f3dc3da69e9f4174e12a0d99828f67c68a
yujunzhen <yujunzheno@163.com>
2018-10-15 20:40:37 +0800

    学习网站第一天

---
+++ baixiu-dev/config.php
@@ -0,0 +1,31 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: yujunzhen
+ * Date: 2018/10/15
+ * Time: 下午8:29
+ */
+/*
+ * 我们项目中用到的配置信息
+ */
+/*
+ * 数据库主机
+ *
+ */
+define('DB_HOST', 'localhost');
+
+/*
+ * 数据库用户名
+ */
+
+define('DB_USER', 'root');
+
+/*
+ * 数据库密码
+ */
+define('DB_PASS', 'Xiu911004');
+
+/*
+ * 数据库名字
+ */
+define('DB_NAME','baixiu');
---
+++ baixiu-dev/index.php
@@ -0,0 +1,6 @@
+<?php
+require_once 'config.php';
+?>
+
+<h1>前台界面</h1>
+<p><?php echo DB_USER?></p>
