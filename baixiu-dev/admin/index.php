21e57e9374cabfc031427f74139102368ce7bf91
yujunzhen <yujunzheno@163.com>
2018-11-01 18:03:07 +0800

    上传一下

--- baixiu-dev/admin/index.php
+++
@@ -1,7 +0,0 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: yujunzhen
- * Date: 2018/10/15
- * Time: 下午8:10
- */
--- baixiu-dev/index.php
+++
@@ -1,5 +0,0 @@
-<?php
-require_once 'config.php';
-?>
-<h1>前台界面</h1>
-<p>hhehhe</p>
