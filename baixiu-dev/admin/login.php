caf28df60d84f7ebadb48afaaa44dde0a8d097d1
于军振 <yujunzheno@163.com>
2019-01-08 01:06:36 +0800

    千呼万唤试出来

--- baixiu-dev/admin/login.php
+++ baixiu-dev/admin/login.php
@@ -1,41 +1,43 @@
 <?php
 include "../config.php";
 session_start();
-$loginError = '登录错误1';
+$message;
 
 function login(){
-
-    $loginError = '登录错误3';
-
+global  $loginError;
+global  $message;
+    $loginErroror = '登录错误3';
     if (empty($_POST['email'])){
-        $globals['meessage'] = '请填写邮箱';
+        $message = '请填写邮箱';
         return;
     }
     if (empty($_POST['password'])){
-        $globals['meessage'] = '请填写密码';
+        $message = '请填写密码';
         return;
     }
     $email = $_POST['email'];
+    echo $email;
     $password = $_POST['password'];
 
     $conn = mysqli_connect("127.0.0.1","root","Xiu911004","baixiu");
     if (!$conn){
         exit('连接数据库失败');
     }
-    $qurey = mysqli_query($conn,"selcet * form users where email ='{$email}' limit 1;");
+    $qurey = mysqli_query($conn,"select * from users where email = '$email' limit 1;");
     if (!$qurey){
-        $globals['meessage'] = '登录失败,请重试';
+        $message = '登录失败,请重试';
         return;
     }
     $user = mysqli_fetch_assoc($qurey);
     if (!$user){
-        $globals['meessage'] = '邮箱与密码不匹配';
+        $message = '邮箱与密码不匹配';
         return;
     }
     $_SESSION['current_login_user'] = $user;
 
     // 一切OK 可以跳转
-   // header('Location: /admin/');
+    $message = '成功了';
+    header('Location: ../admin/');
 
 
 }
@@ -58,13 +60,14 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST'){
 </head>
 <body>
 <div class="login">
-    <?php echo $loginError?>
-    <form class="login-wrap <?php echo isset($message) ? 'shake animated' : 'shake animated' ?>" action="<?php echo $_SERVER['PHP_SELF'];?>" method="POST" autocomplete="off" novalidate>
+    <form class="login-wrap <?php echo isset($message) ? 'shake animated' : '' ?>" action="<?php echo $_SERVER['PHP_SELF'];?>" method="POST" autocomplete="off" novalidate>
         <img class="avatar" src="../assets/img/default.png">
         <!-- 有错误信息时展示 -->
+        <?php if (isset($message)): ?>
        <div class="alert alert-danger">
-          <strong><?php echo $globals['meessage']?></strong>
+          <strong><?php echo $message?></strong>
         </div>
+        <?php endif ?>
         <div class="form-group">
             <label for="email" class="sr-only">邮箱</label>
             <input id="email" name="email" type="email" class="form-control" placeholder="邮箱" autofocus>
@@ -73,7 +76,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST'){
             <label for="password" class="sr-only">密码</label>
             <input id="password" name='password' type="password" class="form-control" placeholder="密码">
         </div>
-        <button class="btn btn-primary btn-block">登 录</button>
+        <button class="btn btn-primary btn-block" type="submit">登 录</button>
     </form>
     <div class="loginError">
         <p><?php echo $loginError ?></p>
