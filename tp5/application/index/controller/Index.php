d196089b1a79eea2a4be3de41dfe1de36b05066f
于军振 <yujunzheno@163.com>
2019-06-05 01:39:36 +0800

    就这样吧

--- tp5/application/index/controller/Index.php
+++ tp5/application/index/controller/Index.php
@@ -10,11 +10,15 @@ class Index extends  Controller
 {
     public function index()
     {
-
+        return $this->fetch();
         return '<style type="text/css">*{ padding: 0; margin: 0; } .think_default_text{ padding: 4px 48px;} a{color:#2E5CD5;cursor: pointer;text-decoration: none} a:hover{text-decoration:underline; } body{ background: #fff; font-family: "Century Gothic","Microsoft yahei"; color: #333;font-size:18px} h1{ font-size: 100px; font-weight: normal; margin-bottom: 12px; } p{ line-height: 1.6em; font-size: 42px }</style><div style="padding: 24px 48px;"> <h1>:)</h1><p> ThinkPHP V5<br/><span style="font-size:30px">十年磨一剑 - 为API开发设计的高性能框架</span></p><span style="font-size:22px;">[ V5.0 版本由 <a href="http://www.qiniu.com" target="qiniu">七牛云</a> 独家赞助发布 ]</span></div><script type="text/javascript" src="https://tajs.qq.com/stats?sId=9347272" charset="UTF-8"></script><script type="text/javascript" src="https://e.topthink.com/Public/static/client.js"></script><think id="ad_bd568ce7058a1091"></think>';
+    }
+    public function indexRequest() {
+
     }
     public function hello()
     {
+        Db::table("book")
         echo 'hello';
     }
     public  function upload(){
@@ -54,7 +58,7 @@ class Index extends  Controller
                 }
 
 
-                $handle = fopen("../public/uploads/".$bookPath, 'r') or die('打开<b>'."../public/uploads/".$bookPath.'</b>文件失败!!');
+                $handle = fopen($bookPath, 'r') or die('打开<b>'.$bookPath.'</b>文件失败!!');
 
                 $data = ['name' => $name, 'author' => $author, 'intro' => $intro, 'type' => $type, 'date' => $date, 'icon' => $icon, 'book_path' => $bookPath];
 
@@ -169,16 +173,16 @@ class Index extends  Controller
    }
 
     public function readbook(){
-        $fileName = "../public/uploads/book/1.txt";
-        $handle = fopen($fileName, 'w') or die('打开<b>'.$fileName.'</b>文件失败!!');
-
-        //循环10次写入10行数据到文件中
-        for($row=0; $row<10; $row++) {
-            //写入文件
-            fwrite($handle, $row.": www.baidu.com\n");
-        }
-        //关闭由fopen()打开的文件指针资源
-        fclose($handle);
+//        $fileName = "../public/uploads/book/1.txt";
+//        $handle = fopen($fileName, 'w') or die('打开<b>'.$fileName.'</b>文件失败!!');
+//
+//        //循环10次写入10行数据到文件中
+//        for($row=0; $row<10; $row++) {
+//            //写入文件
+//            fwrite($handle, $row.": www.baidu.com\n");
+//        }
+//        //关闭由fopen()打开的文件指针资源
+//        fclose($handle);
     }
 
     public function login()
